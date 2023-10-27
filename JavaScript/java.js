dd353208e9af8c31c81fa164dc810aad87135bb9
ycombalia <147435229+ycombalia@users.noreply.github.com>
2023-10-27 12:17:34 +0200

    Update java.js

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -45,13 +45,12 @@ let canviAuto = setInterval(mostraImatges, 3000);
 
 /*____________________________________________Codi del formulari______________________________________*/
 
-/*Declaració i inicialització de la següent funció*/
-    var checkboxes = document.querySelectorAll('.actors');
-    var seleccionats = 0;
-
 //Funció que limita la selecció dels checkbox dels actors i actores a màxim 3. Es cridarà cada cop que a un checkbox dels actors se li canvï el valor és a dir, cada cop que seleccionis i deselecionis una opció
 function limitCheckbox(){
-
+    /*Declaració i inicialització de la següent funció*/
+    var checkboxes = document.querySelectorAll('.actors');
+    var seleccionats = 0;
+    
     /*Bucle que recorre tots els chechbox dels autors per a comprovar quants d'aquests estan seleccionats*/
     for (let i = 0; i <checkboxes.length; i++) {
         if (checkboxes[i].checked) {
